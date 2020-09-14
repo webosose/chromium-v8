95e7e0ad1c13ade26b224b0e522d33be555adc95
soocheol.lee <soocheol.lee@lge.com>
2020-09-14 15:36:03 +0900

    [op][n_upsable][version] Add webOS submission number to v8 version
    :Release Notes:
    Add webOS submission number to v8 version number
    
    :Detailed Notes:
    In order to create a new hash value using v8 version when updating V8
    for webOS, this patch adds webOS submission number to patch level number
    of v8. Because the version information has effects on snapshot, code cache
    and wasm.
    
    Some applications sometimes occur crashes while doing launch due to invalid
    access of a service worker, and it was related with code cache of v8.
    Our thought that there was no way to detect its update status while
    consuming cache data or deserializing snapshot. Because the v8 version
    information is used for a metadata of them. By update the version number,
    v8 can decide if using cached data or not.
    
    :Testing Performed:
    After build complete, check the build/Release/snapshot_blob.bin
    from 9-th byte in the bin file, v8 version is changed as expected.
    
    :QA Notes:
    Depends on: http://gpro.lge.com/c/webos-pro/chromium87/+/305524
    
    :Issues Addressed:
    [PLAT-135166] Port: Add webOS Submission number to v8 version
    
    Originally-Reviewed-on: https://wall.lge.com/c/webosose/chromium-v8/+/288869
    Change-Id: I03cddc2ef281878f5c114e0faebb4b8513a8623d
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium-v8/+/306022
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Jose Dapena Paz <j.dapena@lgepartner.com>
    Reviewed-by: Jeongran Jang <jeongran.jang@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Tested-by: Jeongran Jang <jeongran.jang@lge.com>

--- include/v8-version.h
+++ include/v8-version.h
@@ -11,7 +11,11 @@
 #define V8_MAJOR_VERSION 8
 #define V8_MINOR_VERSION 7
 #define V8_BUILD_NUMBER 220
+#if defined(OS_WEBOS) && defined(WEBOS_SUBMISSION_NUMBER)
+#define V8_PATCH_LEVEL (29*1000 + WEBOS_SUBMISSION_NUMBER)
+#else
 #define V8_PATCH_LEVEL 29
+#endif
 
 // Use 1 for candidates and 0 otherwise.
 // (Boolean macro values are not supported by all preprocessors.)
