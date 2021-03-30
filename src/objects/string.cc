935065d097cce0090bc858746e6aee0919b222de
Oleg Chaklyarov <oleg.chaklyarov@lge.com>
2021-03-30 15:26:00 +0300

    [op][upsable] Fix trace serialisation with std::isprint
    :Release Notes:
    Fix trace serialisation to avoid crashing when std::isprint is used.
    
    :Detailed Notes:
    As follows from std::isprint description like all other functions from
    <cctype> the behavior of std::isprint is undefined if the argument's
    value is neither representable as unsigned char nor equal to EOF.
    To use these functions safely with plain chars (or signed chars)
    the argument should first be converted to unsigned char.
    
    :Testing Performed:
    1. Install com.test.app.extrajs_0.0.2_all.ipk on the board.
    2. Launch the test app.
    Expected Result: ExtraJS Test application should be launched.
    
    :QA Notes:
    Test cases: GT-22756
    
    :Issues Addressed:
    [PLAT-138108] [TV-O20] Crash displayed when ExtraJS TestApp is launched
    
    Change-Id: I116a0479500dd58cb4f6600d0b2fa1ad0762f8b4
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium-v8/+/307378
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Oleg Chaklyarov <oleg.chaklyarov@lge.com>
    Reviewed-by: Jose Dapena Paz <j.dapena@lgepartner.com>
    Reviewed-by: Vladislav Mukulov <vladislav.mukulov@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Tested-by: Oleg Chaklyarov <oleg.chaklyarov@lge.com>

--- src/objects/string.cc
+++ src/objects/string.cc
@@ -379,7 +379,7 @@ void String::PrintUC16(StringStream* accumulator, int start, int end) {
       accumulator->Add("\\r");
     } else if (c == '\\') {
       accumulator->Add("\\\\");
-    } else if (!std::isprint(c)) {
+    } else if (!std::isprint(static_cast<unsigned char>(c))) {
       accumulator->Add("\\x%02x", c);
     } else {
       accumulator->Put(static_cast<char>(c));
